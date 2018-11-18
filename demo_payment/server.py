8fb7a02fea22c3e489f4de6aa70f7e5c40c0c2af
Jack Bott <millerbott@gmail.com>
2018-11-18 04:08:38 -0500

    added initial pages

---
+++ demo_payment/server.py
@@ -0,0 +1,37 @@
+""" Demo Payment Web Server """
+import logging
+from flask import Flask, session, redirect, url_for
+from flask import escape, request, render_template
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route("/")
+def index(name=None):
+    if 'email' in session:
+        return render_template('index.html', name=escape(session['email']))
+    return render_template('index.html', name=name)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login(message=None):
+    if request.method == 'POST':
+        session['email'] = request.form['email']
+        return redirect(url_for('index'))
+    return render_template('login.html', message=None)
+
+@app.route('/logout')
+def logout():
+    print("Logging Out")
+    # remove the email from the session if it's there
+    session.pop('email', None)
+    return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('error.html'), 404
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8888, debug=True)
--- prod/run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-uwsgi -s 127.0.0.1:8888 \
-  --log-prefix=log/demo_payment.log \
-  --protocol=http -w wsgi \
-  --mount /demo_payment=server:app
