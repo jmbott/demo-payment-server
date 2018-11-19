9f7c67385da48acd6937af182d55ea5881c7996c
Jack Bott <millerbott@gmail.com>
2018-11-18 22:35:12 -0500

    initial test

---
+++ tests/python/test_server.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+# Index test alive
+def test_alive():
+    response = requests.get("http://127.0.0.1:8080/")
+    assert response.status_code == 200
+
+# Index test title
+def test_html_title():
+    response = requests.get("http://127.0.0.1:8080/")
+    soup = BeautifulSoup(response.content, "html.parser")
+    assert soup.title.text == "Home"
