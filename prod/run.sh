b43151c8e4ed9b28ad1828bf15e80482ac100aa6
Jack Bott <millerbott@gmail.com>
2018-12-09 23:29:13 -0500

    correct port

--- prod/run.sh
+++ prod/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # python demo_payment/config_create.py --url 'www.example.com'
-python demo_payment/config_create.py --url '127.0.0.1:8889'
+python demo_payment/config_create.py
 uwsgi --yaml wsgi_config.yaml \
   --log-date='%d/%m/%Y-%H:%M:%S' \
-  --logto=logs/uwsgi.log 
+  --logto=logs/uwsgi.log
--- tests/python/test_server.py
+++ tests/python/test_server.py
@@ -4,12 +4,12 @@ import requests
 
 # Index test alive
 def test_alive():
-    response = requests.get("http://127.0.0.1:8080/")
+    response = requests.get("http://127.0.0.1:8889/")
     assert response.status_code == 200
 
 
 # Index test title
 def test_html_title():
-    response = requests.get("http://127.0.0.1:8080/")
+    response = requests.get("http://127.0.0.1:8889/")
     soup = BeautifulSoup(response.content, "html.parser")
     assert soup.title.text == "Home"
