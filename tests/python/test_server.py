ed35c6d5fc0b7ab40319d223de4cb311d05f1226
Jack Bott <millerbott@gmail.com>
2018-12-09 23:46:01 -0500

    options to tests

--- tests/python/test_server.py
+++ tests/python/test_server.py
@@ -1,15 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
 
+from demo_payment.options import options
+
 
 # Index test alive
 def test_alive():
-    response = requests.get("http://127.0.0.1:8889/")
+    response = requests.get("http://" + options.demo_payment_website_url)
     assert response.status_code == 200
 
 
 # Index test title
 def test_html_title():
-    response = requests.get("http://127.0.0.1:8889/")
+    response = requests.get("http://" + options.demo_payment_website_url)
     soup = BeautifulSoup(response.content, "html.parser")
     assert soup.title.text == "Home"
